8b0035661d3a5bc4da2ae3024f7d56cbf8ce0e94
xiaoyufei <915724526@qq.com>
2019-10-06 12:11:29 +0800

    feat : 完善 部分场景

--- src/index.js
+++ src/index.js
@@ -14,15 +14,17 @@ export default function (babel) {
                     const property = node.property
                     // 是否自定义了 RouteNamesChain 变量
                     if (file.scope.getBinding('RouteNamesChain') || !property) return
-                    result += property.name
-                    let preParent = path
-                    let parent = path.findParent((temp) => temp.isMemberExpression())
-                    while (parent && t.isIdentifier(parent.node.property)) {
-                        result += `-${parent.node.property.name}`
+                    let preParent = null
+                    let parent = path
+                    // 要由大写字母开头
+                    while (parent && t.isIdentifier(parent.node.property) && /^[A-Z][a-z]*/.test(parent.node.property.name)) {
+                        result += `${parent.node.property.name}-`
                         preParent = parent
                         parent = parent.findParent((temp) => temp.isMemberExpression())
                     }
-                    preParent.replaceWith(t.stringLiteral(result))
+                    if (preParent) {
+                        preParent.replaceWith(t.stringLiteral(result.slice(0, -1)))
+                    }
                 },
             },
         }
